0bfdacb7617700f3452a09a31ffeb4313b1d64d6
Laura <lstorella10@gmail.com>
2020-06-03 14:28:06 +0200

    modifica html - css

--- js/script.js
+++ js/script.js
@@ -59,14 +59,14 @@ checkPalindrome(userWord);
 
 
 
-// // ESERCIZIO #2 - PARI E DISPARI
-// /* L’utente sceglie pari o dispari e inserisce un numero da 1 a 5
-// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione)
-// Sommiamo i due numeri
-// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-// Dichiariamo chi ha vinto */
-//
-//
+// ESERCIZIO #2 - PARI E DISPARI
+/* L’utente sceglie pari o dispari e inserisce un numero da 1 a 5
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione)
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto */
+
+
 // // RICHIESTA INPUT
 // // Richiesta input user
 // var userNumber = parseInt(prompt('Scegli un numero da 1 a 5'));
